f33e1291315cc422a5c1ebc55de06484fbf71f75
carloand123456-wq <carloand123456@gmail.com>
2026-06-26 15:28:21 -0500

    No hacer cambios, terminado.

--- propuesto1.cpp
+++ propuesto1.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 struct Empleado{
 	string nombres;
-	char sexo;
+	string sexo;
 	float sueldo;
 };
 
 int main(){
-	int n;
+	int n, min, max, pos;
 	Empleado empleados[100];
 	cout<<"Ingrese la cantidad de trabajadores: ";
 	cin>>n;
@@ -26,7 +26,37 @@ int main(){
 		cin.ignore();
 	}
 	
+	max = empleados[1].sueldo;
+	for(int i=1;i<n;i++){
+		if(empleados[i].sueldo>max){
+			max = empleados[i].sueldo;
+			pos = i;
+		}
+	}
 	
+	system("pause");
+	system("cls");
+	cout<<"El empleado con mayor sueldo es: "<<endl;
+	cout<<"\nEmpleado "<<pos+1<<endl;
+	cout<<"Nombres: "<<empleados[pos].nombres<<endl;
+	cout<<"Sexo: "<<empleados[pos].sexo<<endl;
+	cout<<"Sueldo: "<<empleados[pos].sueldo<<endl;
+	
+	min = empleados[1].sueldo;
+	for (int i=1;i<n;i++){
+		if(empleados[i].sueldo<min){
+			min = empleados[i].sueldo;
+			pos = i;
+		}
+	}
+	
+	cout<<"\nEl empleado con menor sueldo es: "<<endl;
+	cout<<"\nEmpleado "<<pos+1<<endl;
+	cout<<"Nombres: "<<empleados[pos].nombres<<endl;
+	cout<<"Sexo: "<<empleados[pos].sexo<<endl;
+	cout<<"Sueldo: "<<empleados[pos].sueldo<<endl;
+	cout<<" "<<endl;
+	system("pause");
 	
 	return 0;
 }
