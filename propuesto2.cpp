240c4055c473a534bfa8149cfbaa08522b23ae22
carloand123456-wq <carloand123456@gmail.com>
2026-06-29 16:33:39 -0500

    Agregamos el bucle para mostrar a cada persona registrada y corregimos algunas cosas.

--- propuesto2.cpp
+++ propuesto2.cpp
@@ -24,10 +24,10 @@ int main(){
 		cout<<"Edad: ";
 		cin>>personas[i].edad;
 		cin.ignore();
+		system("cls");
 	}
 	
-	system("cls");
-	cout<<"Las personas con una edad mayor a 50 son: ";
+	cout<<"Las personas con una edad mayor a 50 son: "<<endl;
 	for(int i=0;i<n;i++){
 		if(personas[i].edad>50){
 			cout<<"\nPersona "<<i+1<<endl;
@@ -36,13 +36,24 @@ int main(){
 			cout<<"Edad: "<<personas[i].edad<<endl;
 		}
 	}
+	system("pause");
 	
 	for(int i=0;i<n;i++){
 		suma = suma + personas[i].edad;
 	}
-	float prom = suma/n;
+	float prom;
+	prom = float(suma)/n;
 	cout<<"\nEl promedio de edades de todas las personas es: "<<prom;
+	system("pause");
 	
+	cout<<"\nLas personas registradas son: "<<endl;
+	for(int i=0;i<n;i++){
+		cout<<"\nPersona "<<i+1<<endl;
+		cout<<"Nombres: "<<personas[i].nombres<<endl;
+		cout<<"DNI: "<<personas[i].DNI<<endl;
+		cout<<"Edad: "<<personas[i].edad<<endl;
+	}
+	system("pause");
 	
 	return 0;
 }
