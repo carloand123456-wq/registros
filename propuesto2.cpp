8570944ae9bc80042cc90b7281f951cb8de8167d
carloand123456-wq <carloand123456@gmail.com>
2026-06-29 15:56:00 -0500

    Funcion para leer la cantidad de personas y bucle para registrar cada una de ellas.

--- propuesto2.cpp
+++ propuesto2.cpp
@@ -9,6 +9,24 @@ struct Persona{
 };
 
 int main(){
+	int n;
+	Persona personas[100];
+	cout<<"Ingrese la cantidad de personas: ";
+	cin>>n;
+	cout<<endl;
+	cin.ignore();
+	
+	for(int i=0;i<n;i++){
+		cout<<"Persona "<<i+1<<endl;
+		cout<<"Nombres: ";
+		getline(cin, personas[i].nombres);
+		cout<<"DNI: ";
+		cin>>personas[i].DNI;
+		cout<<"Edad: ";
+		cin>>personas[i].edad;
+		cin.ignore();
+	}
+	
 	
 	
 	return 0;
