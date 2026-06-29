d7e1e8bdbafef6479a6f95de1bd068783a62817e
carloand123456-wq <carloand123456@gmail.com>
2026-06-29 16:18:05 -0500

    Calculamos el promedio de edades.

--- propuesto2.cpp
+++ propuesto2.cpp
@@ -9,7 +9,7 @@ struct Persona{
 };
 
 int main(){
-	int n;
+	int n, suma;
 	Persona personas[100];
 	cout<<"Ingrese la cantidad de personas: ";
 	cin>>n;
@@ -26,6 +26,8 @@ int main(){
 		cin.ignore();
 	}
 	
+	system("cls");
+	cout<<"Las personas con una edad mayor a 50 son: ";
 	for(int i=0;i<n;i++){
 		if(personas[i].edad>50){
 			cout<<"\nPersona "<<i+1<<endl;
@@ -34,6 +36,13 @@ int main(){
 			cout<<"Edad: "<<personas[i].edad<<endl;
 		}
 	}
+	
+	for(int i=0;i<n;i++){
+		suma = suma + personas[i].edad;
+	}
+	float prom = suma/n;
+	cout<<"\nEl promedio de edades de todas las personas es: "<<prom;
+	
 	
 	return 0;
 }
