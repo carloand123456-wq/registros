063a88c7d276432632c01961e567090bd592b350
carloand123456-wq <carloand123456@gmail.com>
2026-07-01 00:18:21 -0500

    Agregamos el bucle para leer cada atleta del arreglo.

--- propuesto4.cpp
+++ propuesto4.cpp
@@ -10,8 +10,24 @@ struct Atleta{
 };
 
 int main(){
+	int n;
+	Atleta A[100];
+	cout<<"Ingrese el numero de atletas: ";
+	cin>>n;
+	cin.ignore();
 	
-	
+	for(int i=0;i<n;i++){
+		cout<<"Atleta "<<i+1<<endl;
+		cout<<"Nombres: ";
+		getline(cin, A[i].nombres);
+		cout<<"Pais: ";
+		getline(cin, A[i].pais);
+		cout<<"Disciplinas: ";
+		getline(cin, A[i].disciplina);
+		cout<<"Medallas: ";
+		cin>>A[i].medallas;
+		cin.ignore();
+	}
 	
 	
 	return 0;
