4e283b62916896d7099620540ce81875b137d02f
carloand123456-wq <carloand123456@gmail.com>
2026-06-30 23:20:28 -0500

    Agregamos el bucle para leer cada persona del arreglo.

--- propuesto3.cpp
+++ propuesto3.cpp
@@ -10,9 +10,24 @@ struct Persona{
 };
 
 int main(){
-		
+	int n;
+	Persona p[100];
+	cout<<"Ingrese la cantidad de personas: ";
+	cin>>n;
+	cin.ignore();
 	
-	
+	for(int i=0;i<n;i++){
+		cout<<"Persona "<<i+1<<endl;
+		cout<<"Nombres: ";
+		getline(cin, p[i].nombre);
+		cout<<"Dia: ";
+		cin>>p[i].dia;
+		cout<<"Mes: ";
+		cin>>p[i].mes;
+		cout<<"Anio: ";
+		cin>>p[i].anio;
+		cin.ignore();
+	}
 	
 	return 0;
 }
