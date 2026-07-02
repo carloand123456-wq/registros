2cf7b7b59559d22d66d928869d9f5450e56b9a7a
carloand123456-wq <carloand123456@gmail.com>
2026-07-01 23:21:58 -0500

    Agregue la condicion para buscar el que tenga mas medallas segun el pais seleccionado.

--- propuesto4.cpp
+++ propuesto4.cpp
@@ -28,7 +28,7 @@ int main(){
 		getline(cin, A[i].nombres);
 		cout<<"Pais: ";
 		getline(cin, A[i].pais);
-		cout<<"Disciplinas: ";
+		cout<<"Disciplina: ";
 		getline(cin, A[i].disciplina);
 		cout<<"Medallas: ";
 		cin>>A[i].medallas;
@@ -38,6 +38,7 @@ int main(){
 		system("cls");
 	}
 	
+	int pos, max = 0;
 	cout<<"Ingrese un pais: ";
 	getline(cin, pb);
 	cout<<"----------------------------------------"<<endl;
@@ -46,11 +47,23 @@ int main(){
 			cout<<"Atleta "<<"["<<i+1<<"]"<<endl;
 			cout<<"----------------------------------------"<<endl;
 			cout<<"Nombres: "<<A[i].nombres<<endl;
-			cout<<"Disciplinas: "<<A[i].disciplina<<endl;
+			cout<<"Disciplina: "<<A[i].disciplina<<endl;
 			cout<<"Medallas: "<<A[i].medallas<<endl;
 			cout<<"----------------------------------------"<<endl;
+			if(A[i].medallas>max){
+				max = A[i].medallas;
+				pos = i;
+			}
 		}
 	}
+	
+	cout<<"El atleta de "<<pb<<" con mas medallas es: "<<endl;
+	cout<<"----------------------------------------"<<endl;
+	cout<<"Nombres: "<<A[pos].nombres<<endl;
+	cout<<"Disciplina: "<<A[pos].disciplina<<endl;
+	cout<<"Medallas: "<<A[pos].medallas<<endl;
+	cout<<"----------------------------------------"<<endl;
+	
 	system("pause");
 	system("cls");
 	
