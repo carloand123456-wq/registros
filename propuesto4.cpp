8596b20e81ecaf06305ba07283244f44b922f232
carloand123456-wq <carloand123456@gmail.com>
2026-07-01 23:11:18 -0500

    Agregue el bucle y condicion para buscar a cada atleta segun elpais solicitado.

--- propuesto4.cpp
+++ propuesto4.cpp
@@ -11,13 +11,19 @@ struct Atleta{
 
 int main(){
 	int n;
+	string pb;
 	Atleta A[100];
+	cout<<"----------------------------------------"<<endl;
 	cout<<"Ingrese el numero de atletas: ";
 	cin>>n;
+	cout<<"----------------------------------------"<<endl;
 	cin.ignore();
+	system("pause");
+	system("cls");
 	
 	for(int i=0;i<n;i++){
-		cout<<"Atleta "<<i+1<<endl;
+		cout<<"Atleta "<<"["<<i+1<<"]"<<endl;
+		cout<<"----------------------------------------"<<endl;
 		cout<<"Nombres: ";
 		getline(cin, A[i].nombres);
 		cout<<"Pais: ";
@@ -26,9 +32,27 @@ int main(){
 		getline(cin, A[i].disciplina);
 		cout<<"Medallas: ";
 		cin>>A[i].medallas;
+		cout<<"----------------------------------------"<<endl;
 		cin.ignore();
+		system("pause");
+		system("cls");
 	}
 	
+	cout<<"Ingrese un pais: ";
+	getline(cin, pb);
+	cout<<"----------------------------------------"<<endl;
+	for(int i=0;i<n;i++){
+		if(A[i].pais == pb){
+			cout<<"Atleta "<<"["<<i+1<<"]"<<endl;
+			cout<<"----------------------------------------"<<endl;
+			cout<<"Nombres: "<<A[i].nombres<<endl;
+			cout<<"Disciplinas: "<<A[i].disciplina<<endl;
+			cout<<"Medallas: "<<A[i].medallas<<endl;
+			cout<<"----------------------------------------"<<endl;
+		}
+	}
+	system("pause");
+	system("cls");
 	
 	return 0;
 }
